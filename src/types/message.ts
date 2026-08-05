24029fb20b9379c0ba993ed4a8deea743593036e
Darren <sag7825491286@gmail.com>
2026-08-05 15:24:31 +0800

    留言板前端調整

--- src/types/message.ts
+++ src/types/message.ts
@@ -1,13 +1,17 @@
 // 定義留言的 JSON 格式
 export type Message = {
   id: string;
-  users?: { name: string | null }
+  authorId: string;
+  users?: { 
+    name: string | null;
+    image: string | null;
+  };
   content: string;
   createdAt: string;
   channelId: string;
-  /*
-  mentions: [user];
-  referencedFile: file; <- 標記章節內段落用的
-  referencedMessage: message;
+  /* --- 未來預計擴充的功能 --- 
+  mentions?: string[];          // 標記特定使用者 (例如存入 User IDs)
+  referencedFileId?: string;    // 標記章節內段落用的 File ID 或 Block ID
+  referencedMessageId?: string; // 回覆特定留言用的 Message ID
   */
 };
